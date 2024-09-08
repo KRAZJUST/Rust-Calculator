df23e44960192fb537b2f6ccf98d102891012d2e
KRAZJUST <skalkadavid03@gmail.com>
2024-09-08 14:53:53 +0200

    fix: locked the size of calc app

--- project/src/main.rs
+++ project/src/main.rs
@@ -2,13 +2,16 @@ mod app;
 mod calc;
 mod styles;
 
-use eframe::egui::{self, ViewportBuilder};
+use eframe::egui::{self, ViewportBuilder, Vec2};
 use app::CalculatorApp;
 
 fn main() {
     let app = CalculatorApp::default();
     let options = eframe::NativeOptions {
-        viewport: ViewportBuilder::default().with_inner_size([300.0, 385.0]),
+        viewport: ViewportBuilder::default()
+            .with_inner_size([300.0, 385.0])
+            .with_min_inner_size(Vec2::new(300.0, 385.0))
+            .with_min_inner_size(Vec2::new(300.0, 385.0)),
         ..Default::default()
     };
     let _ = eframe::run_native("Calculator", options, Box::new(|_cc| Ok(Box::new(app))));
